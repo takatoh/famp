51214c14ab6930bf6f81131ba55123542db872ea
takatoh <takatoh.m@gmail.com>
2023-02-11 21:17:51 +0900

    main.go(amplitudeAndPhase): Fix with using math.Atan2.

--- main.go
+++ main.go
@@ -106,7 +106,7 @@ func amplitudeAndPhase(a []float64, b []float64, n int) ([]float64, []float64) {
 	for k := 0; k <= n; k++ {
 		xk := math.Sqrt(math.Pow(a[k], 2.0) + math.Pow(b[k], 2.0))
 		amplitude = append(amplitude, xk)
-		phase = append(phase, math.Atan(-b[k]/a[k]))
+		phase = append(phase, math.Atan2(-b[k], a[k]))
 	}
 	return amplitude, phase
 }
